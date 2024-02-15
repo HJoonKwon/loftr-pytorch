b8cc5ab5c0057ead019ca85c7603fbfa39be9ceb
HJoonKwon <dsazz801@gmail.com>
2024-02-15 22:49:29 +0900

    apply config to position_embedding

--- python/loftr_pytorch/model/position_embedding.py
+++ python/loftr_pytorch/model/position_embedding.py
@@ -7,20 +7,25 @@ import math
 # TODO:: Need investigation. Why not using dynamic shape?
 class PositionEmbeddingSine(nn.Module):
     def __init__(
-        self, d_model, max_shape, temperature=10000.0, normalize=True, scale=None
+        self,
+        config 
     ):
         super().__init__()
-        if scale is None:
-            scale = 2 * math.pi
 
-        dim_t = torch.arange(d_model // 2, dtype=torch.float32)
-        dim_t = temperature ** (2 * (dim_t // 2) / d_model)
+        dim = config['dim']
+        temperature = config['temperature']
+        max_shape = config['max_shape']
+        normalize = config['normalize']
+
+        dim_t = torch.arange(dim // 2, dtype=torch.float32)
+        dim_t = temperature ** (2 * (dim_t // 2) / dim)
 
         x_embed = torch.ones(max_shape).cumsum(1).float().unsqueeze(0)
         y_embed = torch.ones(max_shape).cumsum(0).float().unsqueeze(0)
 
         if normalize:
             eps = 1e-6
+            scale = 2 * math.pi
             y_embed = y_embed / (y_embed[:, -1:, :] + eps) * scale
             x_embed = x_embed / (x_embed[:, :, -1:] + eps) * scale
 
