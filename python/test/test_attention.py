b2d60ce431ddb6408f4ea384605f3ffd7650ceea
HJoonKwon <dsazz801@gmail.com>
2024-02-17 17:16:27 +0900

    update unit test for flash/memeff attention

--- python/test/test_attention.py
+++ python/test/test_attention.py
@@ -13,77 +13,44 @@ S = 24 * 24
 n_embd = 32
 n_heads = 4
 
+num_devices = torch.cuda.device_count()
+device = f"cuda:{num_devices-1}" if torch.cuda.is_available() else "cpu"
+
 
 def test_FullAttention():
     attn = FullAttention()
-    q = torch.randn(B, n_heads, L, n_embd // n_heads)
-    k = torch.randn(B, n_heads, S, n_embd // n_heads)
-    v = torch.randn(B, n_heads, S, n_embd // n_heads)
+    q = torch.randn(B, n_heads, L, n_embd // n_heads).to(device)
+    k = torch.randn(B, n_heads, S, n_embd // n_heads).to(device)
+    v = torch.randn(B, n_heads, S, n_embd // n_heads).to(device)
     q_mask = torch.rand(B, L) > 0.1
     kv_mask = torch.rand(B, S) > 0.1
-    y = attn(q, k, v, q_mask, kv_mask)
+    y = attn(q, k, v, q_mask.to(device), kv_mask.to(device))
     assert y.shape == (B, n_heads, L, n_embd // n_heads)
-
-    torch.onnx.export(
-        attn,
-        (q, k, v, q_mask, kv_mask),
-        "./model.onnx",
-        export_params=True,
-        opset_version=17,
-        do_constant_folding=True,
-        input_names=["q", "k", "v", "q_mask", "kv_mask"],
-        output_names=["result"],
-    )
-
-    assert os.path.isfile("./model.onnx")
-    os.remove("./model.onnx")
 
 
 def test_TorchScaleDotProduct():
     attn = TorchScaledDotProduct()
-    q = torch.randn(B, n_heads, L, n_embd // n_heads)
-    k = torch.randn(B, n_heads, S, n_embd // n_heads)
-    v = torch.randn(B, n_heads, S, n_embd // n_heads)
+    q = torch.randn(B, n_heads, L, n_embd // n_heads).to(device)
+    k = torch.randn(B, n_heads, S, n_embd // n_heads).to(device)
+    v = torch.randn(B, n_heads, S, n_embd // n_heads).to(device)
     q_mask = torch.rand(B, L) > 0.1
     kv_mask = torch.rand(B, S) > 0.1
-    y = attn(q, k, v, q_mask, kv_mask)
+
+    # test memory efficient attention
+    y = attn(q, k, v, q_mask.to(device), kv_mask.to(device))
     assert y.shape == (B, n_heads, L, n_embd // n_heads)
 
-    # torch.onnx.export(
-    #     attn,
-    #     (q, k, v, q_mask, kv_mask),
-    #     "./model.onnx",
-    #     export_params=True,
-    #     opset_version=17,
-    #     do_constant_folding=True,
-    #     input_names=["q", "k", "v", "q_mask", "kv_mask"],
-    #     output_names=["result"],
-    # )
-
-    # assert os.path.isfile("./model.onnx")
-    # os.remove("./model.onnx")
+    # test flash attention
+    y = attn(q, k, v, None, None)
+    assert y.shape == (B, n_heads, L, n_embd // n_heads)
 
 
 def test_LinearAttention():
     attn = LinearAttention()
-    q = torch.randn(B, n_heads, L, n_embd // n_heads)
-    k = torch.randn(B, n_heads, S, n_embd // n_heads)
-    v = torch.randn(B, n_heads, S, n_embd // n_heads)
+    q = torch.randn(B, n_heads, L, n_embd // n_heads).to(device)
+    k = torch.randn(B, n_heads, S, n_embd // n_heads).to(device)
+    v = torch.randn(B, n_heads, S, n_embd // n_heads).to(device)
     q_mask = torch.rand(B, L) > 0.1
     kv_mask = torch.rand(B, S) > 0.1
-    y = attn(q, k, v, q_mask, kv_mask)
+    y = attn(q, k, v, q_mask.to(device), kv_mask.to(device))
     assert y.shape == (B, n_heads, L, n_embd // n_heads)
-
-    torch.onnx.export(
-        attn,
-        (q, k, v, q_mask, kv_mask),
-        "./model.onnx",
-        export_params=True,
-        opset_version=17,
-        do_constant_folding=True,
-        input_names=["q", "k", "v", "q_mask", "kv_mask"],
-        output_names=["result"],
-    )
-
-    assert os.path.isfile("./model.onnx")
-    os.remove("./model.onnx")
--- python/test/test_loftr.py
+++ python/test/test_loftr.py
@@ -3,6 +3,8 @@ import yaml
 import torch
 from loftr_pytorch.model.loftr import LoFTR
 
+num_cuda_devices = torch.cuda.device_count()
+device = f"cuda:{num_cuda_devices-1}" if torch.cuda.is_available() else "cpu"
 
 def test_LoFTR():
     config_path = os.path.join(
@@ -10,9 +12,9 @@ def test_LoFTR():
     )
     with open(config_path, "r") as f:
         config = yaml.safe_load(f)
-    model = LoFTR(config)
+    model = LoFTR(config).to(device)
 
-    x0 = torch.rand(1, 1, 256, 256)
-    x1 = torch.rand(1, 1, 256, 256)
+    x0 = torch.rand(1, 1, 256, 256, device=device)
+    x1 = torch.rand(1, 1, 256, 256, device=device)
     data = {"image0": x0, "image1": x1}
     model(data)
