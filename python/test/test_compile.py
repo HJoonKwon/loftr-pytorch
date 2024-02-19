39adc460886389bc24cfe7bd00e01130de13fb27
HJoonKwon <dsazz801@gmail.com>
2024-02-19 23:41:26 +0900

    indices tensor at coarse matcher get assigned to different gpus in multi-gpu env. restrict the gpu number with context manager

--- python/test/test_compile.py
+++ python/test/test_compile.py
@@ -3,6 +3,9 @@ import yaml
 import torch
 from loftr_pytorch.model.loftr import LoFTR
 
+num_cuda_devices = torch.cuda.device_count()
+device = f"cuda:{num_cuda_devices-1}" if torch.cuda.is_available() else "cpu"
+
 
 def test_compile():
     config_path = os.path.join(
@@ -11,8 +14,10 @@ def test_compile():
     )
     with open(config_path, "r") as f:
         config = yaml.safe_load(f)
-    model = LoFTR(config).eval().cuda()
-    model = torch.compile(model)
-    x0 = torch.rand(1, 1, 640, 480).cuda()
-    x1 = torch.rand(1, 1, 640, 480).cuda()
-    model(x0, x1)
+
+    with torch.device(device):
+        model = LoFTR(config).eval().cuda()
+        model = torch.compile(model)
+        x0 = torch.rand(1, 1, 640, 480).cuda()
+        x1 = torch.rand(1, 1, 640, 480).cuda()
+        model(x0, x1)
