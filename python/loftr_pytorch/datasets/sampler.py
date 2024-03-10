b66a43bafd4a123a6ef395228eb218750d293a63
HJoonKwon <dsazz801@gmail.com>
2024-03-10 17:34:04 +0900

    implement distribtued sampler that works with a predefined custom sampler

--- python/loftr_pytorch/datasets/sampler.py
+++ python/loftr_pytorch/datasets/sampler.py
@@ -1,5 +1,109 @@
 import torch
 from torch.utils.data import Sampler, ConcatDataset
+import torch.distributed as dist
+
+
+class DistributedPreSampledSampler(Sampler):
+    def __init__(
+        self,
+        dataset,
+        pre_sampled_indices,
+        num_replicas,
+        rank,
+        shuffle=True,
+        seed=0,
+        pad=True,
+    ):
+        """
+        Args:
+            dataset (torch.utils.data.Dataset): The dataset to sample from
+            pre_sampled_indices (list): List of indices to sample from
+            num_replicas (int, optional): Number of processes participating in distributed training
+            rank (int, optional): Rank of the current process
+            shuffle (bool, optional): Whether to shuffle indices or not
+            seed (int, optional): Seed for shuffling
+            pad (bool, optional): Whether to pad the dataset to ensure each process gets the same number of samples
+
+        Ref:
+            https://pytorch.org/docs/stable/_modules/torch/utils/data/distributed.html#DistributedSampler
+            https://github.com/mlperf/training_results_v0.6/blob/master/NVIDIA/benchmarks/ssd/implementations/pytorch/sampler.py
+            https://github.com/pytorch/pytorch/issues/25162
+        """
+        if num_replicas is None:
+            if not dist.is_available():
+                raise RuntimeError("Requires distributed package to be available")
+            num_replicas = dist.get_world_size()
+        if rank is None:
+            if not dist.is_available():
+                raise RuntimeError("Requires distributed package to be available")
+            rank = dist.get_rank()
+        if rank >= num_replicas or rank < 0:
+            raise ValueError(
+                f"Invalid rank {rank}, rank should be in the interval [0, {num_replicas - 1}]"
+            )
+
+        self.dataset = dataset
+        self.pre_sampled_indices = pre_sampled_indices
+        self.num_replicas = num_replicas
+        self.rank = rank
+        self.shuffle = shuffle
+        self.epoch = 0
+        self.seed = seed
+        self.pad = pad
+
+        if self.pad:
+            # Calculate the total size by rounding up to ensure each replica has the same number of samples
+            self.total_size = (
+                -(-len(self.pre_sampled_indices) // self.num_replicas)
+                * self.num_replicas
+            )
+            # (Padding) Extend the pre_sampled_indices to match the total size if necessary
+            self.pre_sampled_indices += self.pre_sampled_indices[
+                : self.total_size - len(self.pre_sampled_indices)
+            ]
+        else:
+            # No padding; the total size is just the length of pre_sampled_indices
+            # It will be used for validation set to ensure each sample is evaluated
+            self.total_size = len(self.pre_sampled_indices)
+
+        assert self.total_size == len(self.pre_sampled_indices)
+
+        # Calculate the actual number of samples each process should get
+        # If padding is disabled, the number of samples can be different for each process
+        self.num_samples = (
+            self.total_size // self.num_replicas
+            if self.pad
+            else len(self.pre_sampled_indices[self.rank :: self.num_replicas])
+        )
+
+    def __iter__(self):
+        # Generating a seed for shuffling using epoch and external seed
+        g = torch.Generator()
+        g.manual_seed(self.epoch + self.seed)
+
+        if self.pad:
+            # Distributing indices with padding
+            indices = self.pre_sampled_indices[
+                self.rank : self.total_size : self.num_replicas
+            ]
+        else:
+            # Distributing indices without padding, different batch sizes for each GPU are allowed
+            indices = self.pre_sampled_indices[self.rank :: self.num_replicas]
+
+        assert len(indices) == self.num_samples
+
+        if self.shuffle:
+            # Shuffling indices for this replica with epoch and external seed
+            indices = torch.tensor(indices, dtype=torch.long)
+            indices = indices[torch.randperm(len(indices), generator=g)].tolist()
+
+        return iter(indices)
+
+    def __len__(self):
+        return self.num_samples
+
+    def set_epoch(self, epoch):
+        self.epoch = epoch
 
 
 class RandomConcatSampler(Sampler):
